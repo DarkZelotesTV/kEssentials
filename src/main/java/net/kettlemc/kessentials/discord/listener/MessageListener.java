5df4b161cd87b2e9d349fea3c898cdf3cd7a1a87
UeberallGebannt <paulmertens63@gmail.com>
2023-12-29 11:45:55 +0100

    feat: Display role when sending discord message in minecraft

--- src/main/java/net/kettlemc/kessentials/discord/listener/MessageListener.java
+++ src/main/java/net/kettlemc/kessentials/discord/listener/MessageListener.java
@@ -9,10 +9,9 @@ import net.kettlemc.kessentials.Essentials;
 import net.kettlemc.kessentials.config.DiscordConfiguration;
 import net.kettlemc.kessentials.config.Messages;
 import net.kettlemc.kessentials.util.Util;
-import org.bukkit.Bukkit;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 public class MessageListener extends ListenerAdapter {
 
@@ -20,22 +19,23 @@ public class MessageListener extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.isFromType(ChannelType.PRIVATE) || event.getAuthor().isBot() || event.getChannel().getIdLong() != DiscordConfiguration.DISCORD_CHANNEL_ID.getValue())
             return;
+        List<Role> roles;
 
-        List<Role> roles = Objects.requireNonNull(event.getGuild().getMember(event.getAuthor())).getRoles();
+        if (event.getMember() == null) {
+            Essentials.instance().getPlugin().getLogger().warning("Member is null for message: " + event.getMessage().getContentDisplay());
+            roles = Collections.emptyList();
+        } else {
+            roles = event.getMember().getRoles();
+        }
 
         String rank = roles.isEmpty() ? DiscordConfiguration.DEFAULT_RANK.getValue() : Util.stripEmojis(roles.get(0).getName()).trim();
 
-        Bukkit.getOnlinePlayers().forEach(player -> Essentials.instance().messages().sendMessage(player, Messages.DISCORD_CHAT_FORMAT_MINECRAFT,
-                Placeholder.of("name", (ctx, value) -> event.getAuthor().getName()),
-                Placeholder.of("message", (ctx, value) -> event.getMessage().getContentDisplay()),
-                Placeholder.of("rank", (ctx, value) -> rank))
-        );
-        Essentials.instance().messages().sendMessage(Bukkit.getConsoleSender(), Messages.DISCORD_CHAT_FORMAT_MINECRAFT,
+        Essentials.instance().messages().broadcastMessage(Messages.DISCORD_CHAT_FORMAT_MINECRAFT,
+                false,
                 Placeholder.of("name", (ctx, value) -> event.getAuthor().getName()),
                 Placeholder.of("message", (ctx, value) -> event.getMessage().getContentDisplay()),
                 Placeholder.of("rank", (ctx, value) -> rank)
         );
-
-
     }
+
 }
