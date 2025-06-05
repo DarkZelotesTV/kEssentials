61e80f72b1972239152201403fdcaa8bcc74ef33
DarkZelotesTV <113208147+DarkZelotesTV@users.noreply.github.com>
2025-06-06 01:37:16 +0200

    Use JavaPlugin for content manager

--- src/main/java/net/kettlemc/kessentials/content/ContentManager.java
+++ src/main/java/net/kettlemc/kessentials/content/ContentManager.java
@@ -1,23 +1,24 @@
 package net.kettlemc.kessentials.content;
 
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Simple helper for registering commands and listeners.
  */
 public class ContentManager {
 
-    private final Plugin plugin;
+    private final JavaPlugin plugin;
 
-    public ContentManager(Plugin plugin) {
+    public ContentManager(JavaPlugin plugin) {
         this.plugin = plugin;
     }
 
     public void registerCommand(String name, CommandExecutor executor) {
-        var command = plugin.getCommand(name);
+        PluginCommand command = plugin.getCommand(name);
         if (command == null) {
             plugin.getLogger().warning("Command '" + name + "' is not defined in plugin.yml");
             return;
