cc90dbe340e2bd89983caa2ff3d873a69c1a3698
DarkZelotesTV <113208147+DarkZelotesTV@users.noreply.github.com>
2025-06-07 21:33:07 +0200

    Refresh armorsee GUI on armor updates

--- src/main/java/net/kettlemc/kessentials/listener/InventoryClickListener.java
+++ src/main/java/net/kettlemc/kessentials/listener/InventoryClickListener.java
@@ -1,9 +1,13 @@
 package net.kettlemc.kessentials.listener;
 
 import net.kettlemc.kessentials.command.ArmorSeeCommand;
+import net.kettlemc.kessentials.Essentials;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.Bukkit;
 
 public class InventoryClickListener implements Listener {
 
@@ -15,10 +19,24 @@ public class InventoryClickListener implements Listener {
         }
 
         if (event.getClickedInventory().getHolder() instanceof ArmorSeeCommand.ArmorInventoryHolder) {
-            ArmorSeeCommand.ArmorInventoryHolder holder = (ArmorSeeCommand.ArmorInventoryHolder) event.getClickedInventory().getHolder();
             event.setCancelled(true);
-            // TODO: Update the open armor inventories if a player updates their armor
+            return;
+        }
 
+        if (!(event.getWhoClicked() instanceof Player)) {
+            return;
+        }
+
+        Player player = (Player) event.getWhoClicked();
+
+        // Check if the player clicked an armor or offhand slot in their own inventory
+        if (event.getClickedInventory().getHolder() instanceof Player
+                && ((Player) event.getClickedInventory().getHolder()).equals(player)) {
+
+            if (event.getSlotType() == InventoryType.SlotType.ARMOR || event.getSlot() == 40) {
+                Bukkit.getScheduler().runTask(Essentials.instance().getPlugin(), () ->
+                        ArmorSeeCommand.refreshArmorInventories(player));
+            }
         }
 
     }
